5357303afdb5ae9694d98c4260b1103a68fdf7a2
islavstan <islavstan@gmail.com>
2016-09-16 12:10:04 +0300

    16.09.16

--- app/src/main/java/com/islavdroid/blogapp/PostActivity.java
+++ app/src/main/java/com/islavdroid/blogapp/PostActivity.java
@@ -1,15 +1,21 @@
 package com.islavdroid.blogapp;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
@@ -18,8 +24,17 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
+
+import id.zelory.compressor.Compressor;
+import id.zelory.compressor.FileUtil;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 public class PostActivity extends AppCompatActivity {
     private ImageButton mselectImage;
@@ -28,6 +43,8 @@ public class PostActivity extends AppCompatActivity {
     private Button mSubmitBtn;
     private Uri mImagUri = null;
     private ProgressDialog mProgress;
+    private File actualImage;
+    private  Bitmap bm;
 
     private static final int GALLERY_REQUEST = 1;
 
@@ -51,7 +68,7 @@ public class PostActivity extends AppCompatActivity {
         mselectImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //беребрасывает на галерею
+                //перебрасывает на галерею
                 Intent galleryIntent= new Intent(Intent.ACTION_GET_CONTENT);
                 galleryIntent.setType("image/*");
                 startActivityForResult(galleryIntent, GALLERY_REQUEST);
@@ -71,14 +88,16 @@ public class PostActivity extends AppCompatActivity {
         final String tittle_val = mPostTittle.getText().toString().trim();
         final String desc_val = mPostDesc.getText().toString().trim();
 //если поля не пустые и mImagUri есть изображение
-        if (!TextUtils.isEmpty(tittle_val) && !TextUtils.isEmpty(desc_val) && mImagUri !=null) {
+        if (!TextUtils.isEmpty(tittle_val) && !TextUtils.isEmpty(desc_val) && actualImage !=null) {
 
             mProgress.setMessage("Posting to Blog...");
             mProgress.show();
 //в Storage создаём папку Blog_Images для изображений
-            StorageReference filepath = mStorage.child("Blog_Images").child(mImagUri.getLastPathSegment());
-
-            filepath.putFile(mImagUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            StorageReference filepath = mStorage.child("Blog_Images").child(actualImage.getAbsolutePath());
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bm.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] b = baos.toByteArray();
+            filepath.putBytes(b).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
 
@@ -104,8 +123,18 @@ public class PostActivity extends AppCompatActivity {
 
         if(requestCode==GALLERY_REQUEST && resultCode==RESULT_OK){
 
-            mImagUri= data.getData();
 
-            mselectImage.setImageURI(mImagUri);
+            try {
+
+                actualImage = FileUtil.from(this, data.getData());
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                bm=BitmapFactory.decodeFile(actualImage.getAbsolutePath(),options);
+                mselectImage.setImageBitmap(bm);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
         }
     }}
